fb037e407f296fb9647b273b365746f9ff6aab65
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-01-25 20:03:51 +0000

    Update microbit-paddle-mover.py

--- microbit-paddle-mover.py
+++ microbit-paddle-mover.py
@@ -1,1 +1,7 @@
-
+# Imports go at the top
+from microbit import *
+import radio
+radio.config(group=255)
+radio.on()
+radio.send('hello') my_score = 10 
+enemy_score = 0 def is_colliding(paddle_x, paddle_y, ball_x, ball_y):     pass paddle = [0, 0] 
